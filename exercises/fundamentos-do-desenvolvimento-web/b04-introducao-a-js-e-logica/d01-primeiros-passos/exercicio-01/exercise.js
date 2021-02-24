4840587567b4e81d89bec937657a2e218965e94f
Willian Mendes <willianmendesbr@gmail.com>
2021-02-24 15:57:48 -0300

    Finalização do exercicio constantes e variaveis

---
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d01-primeiros-passos/exercicio-01/exercise.js
@@ -0,0 +1,16 @@
+const name = 'Willian Mendes';
+const birthCity = 'Miguelópolis'
+let birthYear = 1995;
+
+console.log('Nome: ' + name);
+console.log('Cidade de Nascimento: ' + birthCity);
+console.log('Ano de Nascimento: ' + birthYear);
+
+birthYear = 2020;
+console.log(birthYear);
+
+/*
+  Uma constante é imutável
+*/
+//birthCity = 'São Paulo';
+//console.log(birthCity);
