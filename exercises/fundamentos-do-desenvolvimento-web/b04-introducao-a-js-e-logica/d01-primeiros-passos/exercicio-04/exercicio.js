f42d56e6c353ffafab16327fcf2851022aa22bac
Willian Mendes <willianmendesbr@gmail.com>
2021-02-24 18:47:37 -0300

    Estruturas condicionais com Switch criadas

--- exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d01-primeiros-passos/exercicio-04/exercicio.js
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d01-primeiros-passos/exercicio-04/exercicio.js
@@ -1,9 +1,27 @@
 let grade = 100;
+let state;
 
 if (grade >= 80) {
+  state = 'aprovado';
   console.log('Parabéns, você foi aprovado(a).');
 } else if (grade < 80 && grade >= 60) {
+  state = 'lista';
   console.log('Você está na nossa lista de espera.');
 } else if (grade < 60) {
+  state = 'reprovado';
   console.log('Você foi reprovado(a)');
+}
+
+switch (state) {
+  case 'aprovado':
+    console.log('Parabéns, você foi aprovado(a).');
+    break;
+  case 'lista':
+    console.log('Você está na nossa lista de espera.');
+    break;
+  case 'reprovado':
+    console.log('Você foi reprovado(a)');
+    break;
+  default:
+    console.log('Não se aplica');
 }
