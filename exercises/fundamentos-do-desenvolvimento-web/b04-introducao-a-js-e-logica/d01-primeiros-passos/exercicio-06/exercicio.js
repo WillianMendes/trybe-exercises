05f85b921b3cf771f95f5210ff7fbaabb07e3504
Willian Mendes <willianmendesbr@gmail.com>
2021-02-24 19:07:21 -0300

    Função que retorna o maior numero entre 3 opções, adicionada

--- exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d01-primeiros-passos/exercicio-06/exercicio.js
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d01-primeiros-passos/exercicio-06/exercicio.js
@@ -1,3 +1,7 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
 function larger(a, b) {
   if (a > b) {
     return a;
@@ -8,4 +12,17 @@ function larger(a, b) {
   }
 }
 
+function largerE(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else if (c > a && c > b) {
+    return c;
+  } else {
+    return null;
+  }
+}
+
 console.log(larger(a, b));
+console.log(largerE(a, b, c))
