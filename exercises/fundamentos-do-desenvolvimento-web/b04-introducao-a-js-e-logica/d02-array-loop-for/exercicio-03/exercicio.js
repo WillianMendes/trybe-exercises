b9261833d92bdc215d59bfcba67d5d102c40c865
Willian Mendes <willianmendesbr@gmail.com>
2021-02-25 20:36:48 -0300

    Programa que descobre o maior numero do array e imprime na tela

--- exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d02-array-loop-for/exercicio-03/exercicio.js
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d02-array-loop-for/exercicio-03/exercicio.js
@@ -29,13 +29,40 @@ console.log('......................................');
  * Exercício 3
  * Para o terceiro exercício, calcule e imprima a média aritmética dos valores
  * contidos no array;
+ *
+ * Exercício 4
+ * Com o mesmo código do exercício anterior, caso valor final seja maior que 20,
+ * imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem:
+ * "valor menor ou igual a 20";
  */
-console.log('Exercício 3 --------------------------');
+console.log('Exercício 3 e 4 --------------------------');
 let media;
 let sumForMedia = 0;
 for (let index = 0; index < numbers.length; index+=1) {
   sumForMedia += numbers[index];
   media = sumForMedia / (numbers.length + 1);
 }
+
+if (media > 20) {
+  console.log('INFO: valor maior que 20');
+} else {
+  console.log('INFO: valor menor ou igual a 20');
+}
+
 console.log('Media de todos os itens do array: ' + media);
+console.log('......................................');
+
+/**
+ * Exercício 5
+ * Para o segundo exercício, você deve somar todos os valores contidos no array
+ * e imprimir o resultado;
+ */
+console.log('Exercício 5 --------------------------');
+let higherNumber = 0;
+for (let index = 0; index < numbers.length; index+=1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
+}
+console.log('O maior numero do array: ' + higherNumber);
 console.log('......................................');
