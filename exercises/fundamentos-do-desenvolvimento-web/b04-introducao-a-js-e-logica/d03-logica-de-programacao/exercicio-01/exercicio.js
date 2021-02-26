c99b7b0c9048c0cf9f0ef4c4fe8a1992751f429e
Willian Mendes <willianmendesbr@gmail.com>
2021-02-26 16:06:10 -0300

    Imprime um quadrado com asteriscos a partir de um valor n

---
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d03-logica-de-programacao/exercicio-01/exercicio.js
@@ -0,0 +1,21 @@
+console.log('......................................');
+console.log('......................................');
+console.log('..... Módulo 1 | Bloco 4 | Dia 3 .....');
+console.log('......................................');
+console.log('......................................n');
+
+/**
+ * Exercício 1
+ * Para o primeiro exercício de hoje, faça um programa que, dado um valor n
+ * qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de
+ * lado de tamanho n
+ */
+let n = 4;
+let line = '';
+for (let i = 0; i < n; i += 1) {
+  line = '';
+  for (let j = 0; j < n; j += 1) {
+    line += '*';
+  }
+  console.log(line);
+}
