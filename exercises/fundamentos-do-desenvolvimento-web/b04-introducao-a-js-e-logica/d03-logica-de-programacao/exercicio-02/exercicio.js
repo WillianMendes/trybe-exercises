ad98268650ddbe772879bc03a84373284206ebc5
Willian Mendes <willianmendesbr@gmail.com>
2021-02-26 16:11:24 -0300

    Imprime um triangulo retangulo com asteriscos a partir de um numero n

--- exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d03-logica-de-programacao/exercicio-02/exercicio.js
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d03-logica-de-programacao/exercicio-02/exercicio.js
@@ -5,17 +5,13 @@ console.log('......................................');
 console.log('......................................');
 
 /**
- * Exercício 1
- * Para o primeiro exercício de hoje, faça um programa que, dado um valor n
- * qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de
- * lado de tamanho n
+ * Exercício 2
+ * Para o segundo exercício, faça o mesmo que antes, mas que imprima um
+ * triângulo retângulo com 5 asteriscos de base.
  */
-let n = 4;
+let n = 5;
 let line = '';
 for (let i = 0; i < n; i += 1) {
-  line = '';
-  for (let j = 0; j < n; j += 1) {
-    line += '*';
-  }
+  line += '*';
   console.log(line);
 }
