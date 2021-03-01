73bc820620249257c538518f3faf36d3f64d5787
Willian Mendes <willianmendesbr@gmail.com>
2021-03-01 19:06:53 -0300

    Novo personagem e impressão dos dois no console

--- exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d04-objetos-e-funcoes/exercicio-01/exercicio.js
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d04-objetos-e-funcoes/exercicio-01/exercicio.js
@@ -48,3 +48,28 @@ for (const infoKey in info) {
   console.log(info[infoKey]);
 }
 console.log('......................................');
+
+/**
+ * Exercício 5
+ * Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do
+ * primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain,
+ * Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+ */
+console.log('Exercício 5 --------------------------');
+let recorrentes = '';
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim"
+};
+if (info.recorrente && info2.recorrente) {
+  recorrentes = 'ambos recorrentes';
+}
+console.log(
+    info.personagem + ' e ' + info2.personagem + '\n' +
+    info.origem + ' e ' + info2.origem + '\n' +
+    info.nota + ' e ' + info2.nota + '\n' +
+    recorrentes
+);
+console.log('......................................');
