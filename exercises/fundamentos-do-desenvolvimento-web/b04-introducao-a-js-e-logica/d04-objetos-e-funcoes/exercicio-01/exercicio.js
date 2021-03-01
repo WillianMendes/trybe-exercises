a210c9362f5054d1d0a61a953850d6f3652be515
Willian Mendes <willianmendesbr@gmail.com>
2021-03-01 18:56:54 -0300

    Melhora a visualização do log

--- exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d04-objetos-e-funcoes/exercicio-01/exercicio.js
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d04-objetos-e-funcoes/exercicio-01/exercicio.js
@@ -9,6 +9,7 @@ console.log('......................................');
  * Imprima no console uma mensagem de boas-vindas para a personagem acima,
  * incluindo seu nome.
  */
+console.log('Exercício 1 --------------------------');
 let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
@@ -16,27 +17,34 @@ let info = {
 };
 
 console.log('Bem-vinda ' + info.personagem);
+console.log('......................................');
 
 /**
  * Exercício 2
  * Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o
  * valor "Sim" e, em seguida, imprima o objeto no console.
  */
+console.log('Exercício 2 --------------------------');
 info['recorrente'] = 'Sim';
 console.log(info);
+console.log('......................................');
 
 /**
  * Exercício 3
  * Faça um for/in que mostre todas as chaves do objeto.
  */
+console.log('Exercício 3 --------------------------');
 for (const infoKey in info) {
   console.log(infoKey);
 }
+console.log('......................................');
 
 /**
  * Exercício 4
  * Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
  */
+console.log('Exercício 4 --------------------------');
 for (const infoKey in info) {
   console.log(info[infoKey]);
 }
+console.log('......................................');
