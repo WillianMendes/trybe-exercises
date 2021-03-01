b4b46cb68f5b4c33013f7876790e0e263a965329
Willian Mendes <willianmendesbr@gmail.com>
2021-03-01 19:36:04 -0300

    Função que recebe um array de inteiros e retorna o indice do maior valor

--- exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d04-objetos-e-funcoes/exercicio-02/exercicio.js
+++ exercises/fundamentos-do-desenvolvimento-web/b04-introducao-a-js-e-logica/d04-objetos-e-funcoes/exercicio-02/exercicio.js
@@ -29,3 +29,24 @@ function checkIsPalindromo(name) {
 console.log(checkIsPalindromo('arara'));
 console.log('......................................');
 
+/**
+ * Exercício 7
+ * Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+ * Array de teste: [2, 3, 6, 7, 10, 1]; .
+ * Valor esperado no retorno da função: 4 .
+ */
+console.log('Exercício 7 --------------------------');
+function returnIndexLargestValue (arrayInteger) {
+  let keyLargestValue = 0;
+
+  for (let index = 0; index < arrayInteger.length; index += 1) {
+    if(arrayInteger[index] >= arrayInteger[keyLargestValue]) {
+      arrayInteger[keyLargestValue] = arrayInteger[index];
+      keyLargestValue = index;
+    }
+  }
+  return keyLargestValue;
+}
+let numbers = [2, 3, 6, 7, 10, 1];
+console.log(returnIndexLargestValue(numbers));
+console.log('......................................');
