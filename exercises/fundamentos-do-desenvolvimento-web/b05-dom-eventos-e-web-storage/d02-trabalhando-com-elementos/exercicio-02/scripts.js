335767d6ff0070fd41f5e907d42ffc3d57eefb75
Willian Mendes <willianmendesbr@gmail.com>
2021-03-11 18:57:56 -0300

    Exercicio 2

--- exercises/fundamentos-do-desenvolvimento-web/b05-dom-eventos-e-web-storage/d02-trabalhando-com-elementos/exercicio-02/scripts.js
+++ exercises/fundamentos-do-desenvolvimento-web/b05-dom-eventos-e-web-storage/d02-trabalhando-com-elementos/exercicio-02/scripts.js
@@ -11,3 +11,10 @@ console.log('......................................');
 const title = document.createElement('h1');
 title.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.getElementsByTagName('body')[0].appendChild(title);
+
+/**
+ * Adicione a tag div com a classe main-content como filho da tag body ;
+ */
+const divEx2 = document.createElement('div');
+divEx2.classList.add('main-content');
+document.getElementsByTagName('body')[0].appendChild(divEx2);
