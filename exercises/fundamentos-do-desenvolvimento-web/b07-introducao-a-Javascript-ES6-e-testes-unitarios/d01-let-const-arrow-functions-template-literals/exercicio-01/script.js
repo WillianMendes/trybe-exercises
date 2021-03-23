a0bbcb0138ccd690c5686eaaaaf65ca35bc5f857
Willian Mendes <willianmendesbr@gmail.com>
2021-03-23 15:38:15 -0300

    Exercicio 01 - passo 1

---
+++ exercises/fundamentos-do-desenvolvimento-web/b07-introducao-a-Javascript-ES6-e-testes-unitarios/d01-let-const-arrow-functions-template-literals/exercicio-01/script.js
@@ -0,0 +1,23 @@
+console.log('......................................');
+console.log('......................................');
+console.log('..... Módulo 1 | Bloco 7 | Dia 1 .....');
+console.log('......................................');
+console.log('......................................');
+
+/**
+ * Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+ * Modifique a estrutura da função para que ela seja uma arrow function .
+ * Modifique as concatenações para template literals .
+ */
+function testingScope(escopo) {
+    if (escopo === true) {
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+        console.log(ifScope);
+    } else {
+        const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+    }
+}
+
+testingScope(true);
