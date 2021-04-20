26f789ef56f0b415ab77f2a29fd3fdfd15e870ea
Willian Mendes <willianmendesbr@gmail.com>
2021-04-20 19:12:42 -0300

    Se a informação que se quer acessar não existir, a função deve retornar o erro "Informação indisponível" - implementado

--- exercises/fundamentos-do-desenvolvimento-web/b10-testes-automatizados-jest/d01-primeiros-passos-jest/exercicio-06/script-tdd.test.js
+++ exercises/fundamentos-do-desenvolvimento-web/b10-testes-automatizados-jest/d01-primeiros-passos-jest/exercicio-06/script-tdd.test.js
@@ -7,4 +7,7 @@ describe('Testa a função searchEmployee', function () {
     it('Verifica se a função retorna ID não identificada quando o ID não consta no quadro de funcionários', function () {
         expect(searchEmployee('0000', ['firstName, lastName'])).toBe('ID não identificada');
     });
+    it('Se a informação que se quer acessar não existir, a função deve retornar o erro "Informação indisponível"', function () {
+        expect(searchEmployee('8579-6', ['firstName, lastName, motherName'])).toBe('Informação indisponível');
+    });
 });
