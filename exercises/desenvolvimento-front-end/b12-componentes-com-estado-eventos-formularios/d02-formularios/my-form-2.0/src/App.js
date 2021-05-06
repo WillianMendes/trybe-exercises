10ba1b6df8e1e52f545fceb0ab5a5eec849ae3aa
Willian Mendes <willianmendesbr@gmail.com>
2021-05-06 06:35:22 -0300

    exercicio 11 - displays curriculum data

--- exercises/desenvolvimento-front-end/b12-componentes-com-estado-eventos-formularios/d02-formularios/my-form-2.0/src/App.js
+++ exercises/desenvolvimento-front-end/b12-componentes-com-estado-eventos-formularios/d02-formularios/my-form-2.0/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Form from './form/form';
+import '../node_modules/@popperjs/core/dist/esm/popper';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min';
 
 function App() {
   return (
